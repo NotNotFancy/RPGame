11cea954da26c32752447be200b38b149d137223
NotNotFancy <theonlymjrf@gmail.com>
2025-05-30 11:17:04 -0400

    Speed

--- movement.gd
+++ movement.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
-var speed = 400  # speed in pixels/sec
+var speed = 1000000000000  # speed in pixels/sec
 
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_pressed("ui_accept"):
 		speed = 800
 	else:
